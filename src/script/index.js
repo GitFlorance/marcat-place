ceaa707bd1f3518ad6b82c806d29ec4d430309fa
Suembike <Simochka.violet@mail.ru>
2022-12-02 19:28:41 +0300

    fix webpack.config

---
+++ src/script/index.js
@@ -0,0 +1,2 @@
+import '@/image/logo1.png';
+import '@/styles/index.pcss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,95 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const miniCss = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const postcssNested = require('postcss-nested');
+const postcssImport = require('postcss-import');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    app: './src/scripts/main.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: path.resolve(__dirname, 'src/scripts'),
-      },
-      {
-        test: /\.scss$/,
-        use: [  miniCss.loader, "css-loader", "sass-loader"]
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'fonts'
-          },
-        }],
-      },
-      {
-        test: /\.(jpg|png)$/,
-        include: path.resolve(__dirname, 'src/img'),
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'img'
-          },
-        }],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [ '.js','.scss','.png','jpg' ],
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-
-  devServer: {
-    compress: true,
-    historyApiFallback: true,
-    port: 3000,
-    hot: "only",
-    static: {
-      directory: './dist',
+    mode: 'development',
+    entry: {
+        app: './src/script',
     },
-    client: {
-      overlay: true,
-    }
-  },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: path.resolve(__dirname, 'src/script'),
+            },
+            {
+                test: /\.(css|pcss)$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            postcssOptions: {
+                                plugins: [postcssNested,postcssImport],
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts'
+                    },
+                }],
+            },
+            {
+                test: /\.(jpg|png)$/,
+                include: path.resolve(__dirname, 'src/image'),
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'image'
+                    },
+                }],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.scss', '.png', 'jpg'],
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
 
-  plugins: [
-    new CleanWebpackPlugin(),
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 3000,
-    }),
-    new HtmlWebpackPlugin({template: './src/index.html'}),
-    new miniCss({
-      filename: 'style.css',
-   }),
-  ],
-  
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'js/[name].bundle.js',
-    chunkFilename: 'js/[id].chunk.js'
-  },
+    devServer: {
+        compress: true,
+        historyApiFallback: true,
+        port: 3000,
+        hot: "only",
+        static: {
+            directory: './dist',
+        },
+        client: {
+            overlay: true,
+        }
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+        }),
+        new HtmlWebpackPlugin({template: './src/pages/index.html'}),
+    ],
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'js/[name].bundle.js',
+        chunkFilename: 'js/[id].chunk.js'
+    },
 };
